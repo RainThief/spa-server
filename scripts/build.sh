8f499b288271b4a9b85b13f827ad5ccc604f817a
Marty <martinfleming@live.co.uk>
2020-06-05 12:40:36 +0000

    ci static anaysis seperate stage

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,9 @@ TAG="$CI_REGISTRY_IMAGE":"${CI_COMMIT_REF_NAME//\//--}"
 docker build -t $TAG .
 
 if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
-    NEW_TAG="$(git_tag)"
+    # NEW_TAG="$(git_tag)"
     docker tag "$TAG" "$CI_REGISTRY_IMAGE":latest
-    docker tag "$TAG" "$CI_REGISTRY_IMAGE":"${NEW_TAG/v/}"
+    # docker tag "$TAG" "$CI_REGISTRY_IMAGE":"${NEW_TAG/v/}"
 fi
 
 docker login $CI_REGISTRY -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN
