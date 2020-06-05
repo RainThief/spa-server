082c939a255d94d75433ef27e79cd2a685838180
Marty <martinfleming@live.co.uk>
2020-06-05 10:34:56 +0000

    added static analysis to ci

--- run_static_analysis.sh
+++ run_static_analysis.sh
@@ -5,3 +5,4 @@ set -e
 PROJECT_ROOT="$(cd "$(dirname "$0")" && pwd)"
 
 docker run --rm -v "$PROJECT_ROOT":/app -t -w /app golangci/golangci-lint:v1.27.0 golangci-lint run -E golint -D gosimple
+# @todo echo green
