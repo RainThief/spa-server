6e9a04d70294a13fcd008308e18ce349d06449b4
Marty <martinfleming@live.co.uk>
2020-06-17 16:26:57 +0000

    refactor to correct app structure

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -e
 
 # @todo gen ssl certs
 
-docker build -t tmp .
+docker build -f build/Dockerfile -t tmp .
 
-docker run --init --rm -it -p 80:80 -p 443:443 -v $(pwd)/config.default.yaml:/config.yml --name tmp tmp $@
+docker run --init --rm -it -p 80:80 -p 443:443 -v $(pwd)/configs/config.default.yaml:/config.yml --name tmp tmp $@
 
 # @todo update readme with where certs arety
