1e03ef46d1825d6bb1759533710709783e5d9521
Martin Fleming <martinfleming@live.co.uk>
2021-03-30 15:40:43 +0000

    tidy up

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -3,16 +3,9 @@
 # Assume this script is in the src directory and work from that location
 PROJECT_ROOT=$(cd "$(dirname $0)" && pwd)
 
-# if [[ ! -e .gomodules ]]; then
-#     mkdir .gomodules
-# fi
-
-    # -e GOPATH=/gopath -v $(pwd)/.gomodules:/gopath \
-
-
 docker run --rm  -t \
     -u $(id -u):$(id -g) \
     --mount type=bind,source="${PROJECT_ROOT}",target=/app \
     -w /app \
     -e XDG_CACHE_HOME=/tmp/.cache \
-    golang:1.14 go test -race ./... "$@"
+    golang:1.14 go test -race -cover ./cmd/... "$@"
